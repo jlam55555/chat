6b458a4124d64ec7ff82a9d2ac32ce0e5c5482a0
Jonathan Lam <jlam55555@gmail.com>
2016-01-02 14:58:23 -0500

    Changed index.js

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var app = express();
 app.set("port", 1234);
 
 app.use(express.static(__dirname + "/index.html"));
+
+app.get('/', function(request, response) {
+  response.write("Hello, World!");
+});
