5f50b78a59c76bdbdb76137130a9d55ad4c74aaf
Jonathan Lam <jlam55555@gmail.com>
2016-01-02 15:16:16 -0500

    Did something

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-/*var express = require("express");
+var express = require("express");
 var app = express();
 
-app.set("port", 5000);
-
-app.use(express.static(__dirname + "/index.html"));
-
+app.set("port", (process.env.PORT || 5000));
 app.get('/', function(request, response) {
-  response.write("Hello, World!");
-});*/
+  response.sendFile(__dirname + "index.html");
+});
 
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
-});
-
-var server = app.listen((process.env.PORT || 5000), function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
 });
