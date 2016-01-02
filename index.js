90538e422a27df6bdd58da24e95f4463928f57cf
Jonathan Lam <jlam55555@gmail.com>
2016-01-02 18:30:08 -0500

    Created realtime chatting

--- index.js
+++ index.js
@@ -6,10 +6,23 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+var clients = [];
+var clientNicknames = [];
 io.on("connection", function(socket) {
-  socket.broadcast.emit("newuser");
-  socket.on("message", function(message) {
-    io.emit("message", message);
+  socket.on("nickname", function(nick) {
+    socket.broadcast.emit("newuser", nick);
+    clients.push(socket);
+    clientNicknames.push(nick);
+  });
+  socket.on("message", function(message, nickname) {
+    socket.broadcast.emit("message", message, nickname);
+    socket.emit("yourMessage", message);
+  });
+  socket.on("disconnect", function() {
+    var i = clients.indexOf(socket);
+    io.emit("disconnect", clientNicknames[i]);
+    clients.splice(i, 1);
+    clientNicknames.splice(i, 1);
   });
 });
 
