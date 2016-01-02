f9607e25a1ff833c718a048c3fd4e146c4da68bd
Jonathan Lam <jlam55555@gmail.com>
2016-01-02 15:20:16 -0500

    Trying yet again

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var express = require("express");
+var express = require('express');
 var app = express();
 
-app.set("port", (process.env.PORT || 5000));
-app.get('/', function(request, response) {
+app.get('/', function (req, res) {
   response.sendFile(__dirname + "/index.html");
+});
+
+var server = app.listen((process.env.PORT || 5000), function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
 });
