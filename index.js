bb536f3e5dc624563e0e70f0889d95e6454d7d66
Jonathan Lam <jlam55555@gmail.com>
2016-01-02 15:24:28 -0500

    Testing

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-var server = app.listen((process.env.PORT || 5000), function () {
+http.listen((process.env.PORT || 5000), function() {
+  console.log("testing");
+});
+
+/*var server = app.listen((process.env.PORT || 5000), function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
-});
+});*/
