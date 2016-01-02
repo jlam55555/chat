7f1b3741f1534849806b933c1d8efe27fe9be764
Jonathan Lam <jlam55555@gmail.com>
2016-01-02 15:25:25 -0500

    Testing again

--- index.js
+++ index.js
@@ -6,13 +6,9 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-http.listen((process.env.PORT || 5000), function() {
-  console.log("testing");
-});
-
-/*var server = app.listen((process.env.PORT || 5000), function () {
+var server = app.listen((process.env.PORT || 5000), function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
-});*/
+});
