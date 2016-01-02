79033a688880c7175bf8cf31a64145e02f7357d6
Jonathan Lam <jlam55555@gmail.com>
2016-01-02 15:18:46 -0500

    Trying again

--- index.js
+++ index.js
@@ -5,10 +5,3 @@ app.set("port", (process.env.PORT || 5000));
 app.get('/', function(request, response) {
   response.sendFile(__dirname + "/index.html");
 });
-
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
