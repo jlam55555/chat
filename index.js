cd6187f5b3f64fd83f123d9383bac9a87291585e
Jonathan Lam <jlam55555@gmail.com>
2016-01-02 15:21:31 -0500

    Fixed error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  response.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 var server = app.listen((process.env.PORT || 5000), function () {
