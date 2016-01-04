7d72d0f45aa1413c8aa353a55d00760e594dcb94
Jonathan Lam <jlam55555@gmail.com>
2016-01-03 21:48:39 -0500

    Split into multiple files

--- index.js
+++ index.js
@@ -3,10 +3,7 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + "/public"));
-/*app.get('/', function (req, res) {
-  res.sendFile(__dirname + "/public/index.html");
-});*/
+app.use(require("express").static(__dirname + "/public"));
 
 var clients = [];
 var clientNicknames = [];
