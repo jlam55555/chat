8226968959243232eb4642883a066ddf99887d1f
Jonathan Lam <jlam55555@gmail.com>
2016-01-03 21:41:02 -0500

    Added public folder

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var app = require('express')();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + "/public"));
+/*app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});*/
 
 var clients = [];
 var clientNicknames = [];
